402ea6f7eb2121db4ecf9dcdcf6446647a9d980c
AlexGoodD <alejandrochavarb@gmail.com>
2024-12-11 01:02:21 -0600

    package.json edit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [vue(), vueJsx(), vueDevTools()],
@@ -13,5 +10,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  server: {
+    port: 5173,
   },
 })
