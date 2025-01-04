29e8a4b28ea13dc2c65db3215f0d8e5735689703
AlexGoodD <alejandrochavarb@gmail.com>
2025-01-03 19:42:29 -0600

    Emision estado de lenguaje global

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    <Navbar />
+    <Navbar @language-changed="handleLanguageChange" />
     <main class="main-content">
-      <router-view />
+      <router-view :language="language" />
     </main>
-    <Footer ref="footer" />
+    <Footer />
   </div>
 </template>
+
 <script setup lang="ts">
+import { ref } from 'vue'
 import Navbar from './components/AppNavbar.vue'
 import Footer from './components/AppFooter.vue'
+
+const language = ref('es')
+
+const handleLanguageChange = (newLang: string) => {
+  language.value = newLang
+}
 </script>
